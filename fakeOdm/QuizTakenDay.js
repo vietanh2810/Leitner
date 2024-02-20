aa0a8b7d90e17374835e9da81b0aa11181aa6463
DrAtsiSama <dareka@outlook.fr>
2024-02-20 18:55:20 +0100

    comments

--- fakeOdm/QuizTakenDay.js
+++ fakeOdm/QuizTakenDay.js
@@ -7,12 +7,21 @@ const __dirname = dirname(__filename);
 
 const quizTakenDaysFilePath = join(__dirname, '..', 'data', 'quizTakenDays.json');
 
+/**
+ * Return boolean on used day
+ * @param  day 
+ * @returns boolean
+ */
 const checkIfDayIsAlreadyTaken = (day) => {
     const quizTakenDaysData = readFileSync(quizTakenDaysFilePath);
     const quizTakenDays = JSON.parse(quizTakenDaysData);
     return quizTakenDays.includes(day);
 }
 
+/**
+ * set Day as taken
+ * @param  day 
+ */
 const markDayAsTaken = (day) => {
     const quizTakenDaysData = readFileSync(quizTakenDaysFilePath);
     const quizTakenDays = JSON.parse(quizTakenDaysData);
